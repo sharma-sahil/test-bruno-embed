eef26cda82807add7daad2509c7110efd2c3a2f1
Sahil-Sharma1_cvent <sahil.sharma1@cvent.com>
2025-08-05 12:29:51 +0530

    first commit

---
+++ src/main/java/com/sharma/sahil/bruno/TestBrunoEmbedApplication.java
@@ -0,0 +1,13 @@
+package com.sharma.sahil.bruno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBrunoEmbedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBrunoEmbedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharma/sahil/bruno/TestBrunoEmbedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharma.sahil.bruno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBrunoEmbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
